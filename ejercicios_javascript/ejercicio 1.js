bfe200ea91b8f69ffcfbc0d64782019a346d19c6
rodrigo villca <villcarodrigo70@gmail.com>
2023-08-15 17:09:17 -0300

    programacion web js

---
+++ ejercicios_javascript/ejercicio 1.js
@@ -0,0 +1,34 @@
+//1) se va a ejetutar el array pero con el siguien cambio de 1 a 10 y de 3 a 30
+
+//2)
+//const array = ["nombre", "hamburguesa", "papas"];
+
+//function procesarPedido(pedidos) {
+// const nombre = pedidos.shift(); //borra la primera posicion del array pasado por parametro y es almacenado dentro de la variable nombre.
+//pedidos.unshift("bebida"); //agrega al principio del array ‘bebida’
+//pedidos.push(nombre); //agrega al final del array la variable nombre que contiene el nombre que estaba ubicado en la primera posicion del array que le pasamos por parametro
+//console.log(pedidos);
+//return pedidos; //retorno de la funcion
+//}
+
+//console.log(procesarPedido(array));
+
+//const numero = [1, 2, 3, 4, 5, 6];
+//let suma = 0;
+//function sumarPares(numeros) {
+//  for (let i = 0; i < numeros.length; i++) {
+//recorre el array
+//    if (numeros[i] % 2 === 0) {
+//      // esta es la condocion
+//      suma = suma + numeros[i]; // suma guarda los valores de la suma de los numeros que complieron con la condicion
+//    }
+//  }
+//  console.log(suma); //me muestra la suma de la condicion
+//}
+//sumarPares(numero); //llamo a la funcion usando de parametro mi array
+
+let palabras = ["computadora", "botella", "celular", "agua"];
+
+function terminanEnA(words) {
+  for (let i = 0; i < numeros.length; i++) {}
+}
