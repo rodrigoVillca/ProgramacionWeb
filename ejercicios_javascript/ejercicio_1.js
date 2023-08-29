dfd7bc82698e6a13bea83db1ff60bf71bde710d9
rodrigo villca <villcarodrigo70@gmail.com>
2023-08-29 16:58:27 -0300

    termine la primera parte de objetos ahorra me falta la segunda

--- ejercicios_javascript/ejercicio_1.js
+++ ejercicios_javascript/ejercicio_1.js
@@ -39,20 +39,75 @@ console.log(terminanEnA(palabras));
 */
 //5)
 
-let numeros = [-1, 5, -3, -9, 7, 4, -8, 2]; // creo
+//let numeros = [-1, 5, -3, -9, 7, 4, -8, 2]; // creo el array
 
-function sortAbsoluteNumbers(numbers) {
-  let absolute = [];
+//function sortAbsoluteNumbers(numbers) {
+//let absolute = [];
 
-  for (let i = 0; i < numbers.length; i++) {
-    //recorro el array
-    let absolute_number = Math.abs(numbers[i]); // abolute_number guarda todo lo que hizo el metodo "math abs" con cada numero de mi array
-    absolute.push(absolute_number); // aca despues de crear mi lista vacia que es "absolute" lo que hice fue que los valores que fue cambiando la funcion "math abs" se guarden con push en mi lista vacia
+//for (let i = 0; i < numbers.length; i++) {
+//recorro el array
+//let absolute_number = Math.abs(numbers[i]); // abolute_number guarda todo lo que hizo el metodo "math abs" con cada numero de mi array
+//absolute.push(absolute_number); // aca despues de crear mi lista vacia que es "absolute" lo que hice fue que los valores que fue cambiando la funcion "math abs" se guarden con push en mi lista vacia
+//}
+//let resultado = absolute.sort(function (a, b) {
+//el absolute.sort ordena de menor a mayor los valores de mi array
+//return a - b;
+// });
+//return resultado;
+//}
+//console.log(sortAbsoluteNumbers(numeros));
+
+//6)
+
+//const words = [
+//  "hola",
+//  "tres",
+//  "uno",
+//  "Universidad",
+//  "de",
+//  "Buenos",
+//  "Aires",
+///  "pepito",
+//  "celular",
+//];
+//const posicionpepito = words.indexOf("pepito");
+//console.log(posicionpepito); //7
+//const palabras = [
+//"hola",
+//"tres",
+//"uno",
+/*"Universidad",
+  "de",
+  "Buenos Aires",
+  "pepito",
+  "celular",
+];
+const palabra = "pepito";
+
+function buscaPalabras(words, word) {
+  let mayorSiete = [];
+  const posicionpepito = words.indexOf(word);
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > posicionpepito) {
+      mayorSiete.push(words[i]);
+    }
   }
-  let resultado = absolute.sort(function (a, b) {
-    //el absolute.sort ordena de menor a mayor los valores de mi array
-    return a - b;
-  });
-  return resultado;
+  console.log(mayorSiete);
 }
-console.log(sortAbsoluteNumbers(numeros));
+buscaPalabras(palabras, palabra);
+*/
+function createObject(name, subs) {
+  const Objeto = {
+    name: name,
+    suscribers: subs,
+    hash: name.length * subs,
+    getStatus: function () {
+      return `el canal de ${name} tiene ${subs}`;
+    },
+  };
+  return Objeto;
+}
+
+//const persona = createObject("rodrigo", 30).name;
+const persona = createObject("rodrigo", 30);
+console.log(persona);
